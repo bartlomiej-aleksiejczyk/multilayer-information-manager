c007fa1696653611c560713b23f8b9567af8ca93
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-11-09 02:24:24 +0100

    Add initial django prototype project

---
+++ prototype/core/information_manager_base/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InformationManagerBaseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'information_manager_base'
